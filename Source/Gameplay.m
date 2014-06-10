dd0edcd5f03c547178b204ec7c08349ad1bc92dc
tingyu <ytcleartime@gmail.com>
2014-06-09 22:47:18 -0700

    follow scene and whack ice blocks

--- Source/Gameplay.m
+++ Source/Gameplay.m
@@ -40,6 +40,12 @@
     CGPoint launchDirection = ccp(1, 0);
     CGPoint force = ccpMult(launchDirection, 8000);
     [penguin.physicsBody applyForce:force];
+    
+    // ensure followed object is in visible are when starting
+    self.position = ccp(0, 0);
+    CCActionFollow *follow = [CCActionFollow actionWithTarget:penguin worldBoundary:self.boundingBox];
+    [self runAction:follow];
+
 }
 
 @end
