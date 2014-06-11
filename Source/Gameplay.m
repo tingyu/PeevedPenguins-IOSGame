6c565d9f087c85757b5555c12f5b7f4a223358ec
tingyu <ytcleartime@gmail.com>
2014-06-10 18:28:12 -0700

    upright catapult

--- Source/Gameplay.m
+++ Source/Gameplay.m
@@ -46,7 +46,7 @@
     // loads the Penguin.ccb we have set up in Spritebuilder
     CCNode* penguin = [CCBReader load:@"Penguin"];
     // position the penguin at the bowl of the catapult
-    penguin.position = ccpAdd(_catapultArm.position, ccp(16, 50));
+    penguin.position = ccpAdd(_catapultArm.position, ccp(16, 150));
     
     // add the penguin to the physicsNode of this scene (because it has physics enabled)
     [_physicsNode addChild:penguin];
