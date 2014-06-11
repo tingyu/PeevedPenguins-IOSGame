966250bf822120a55d908253dbbe5b5ca8abe86f
tingyu <ytcleartime@gmail.com>
2014-06-10 20:34:46 -0700

    Unsynchronize the animations

---
+++ Source/WaitingPenguin.h
@@ -0,0 +1,13 @@
+//
+//  WaitingPenguin.h
+//  PeevedPenguins
+//
+//  Created by Ting on 6/10/14.
+//  Copyright (c) 2014 Apportable. All rights reserved.
+//
+
+#import "CCSprite.h"
+
+@interface WaitingPenguin : CCSprite
+
+@end
